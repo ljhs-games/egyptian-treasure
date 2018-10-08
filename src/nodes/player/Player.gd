a16a165a2eaa137bb45e536d7afe854dc8f9e812
Cameron Reikes <cameronreikes@gmail.com>
2018-10-07 21:32:30 -0700

    Add basic player physics and controls

--- src/nodes/player/Player.gd
+++ src/nodes/player/Player.gd
@@ -3,7 +3,8 @@ extends KinematicBody2D
 enum ACTIONS { right, left }
 var cur_actions = { ACTIONS.right: 0, ACTIONS.left: 0 }
 
-export var move_speed = 300.0
+export var move_acceleration = 20.0
+export var max_move_velocity = 40.0
 export var gravity = 9.8
 
 var velocity = Vector2()
@@ -15,7 +16,22 @@ func _ready():
 	set_process_input(true)
 
 func _physics_process(delta):
-	move_and_slide(velocity, Vector2(0, -1))
+	acceleration.x = (cur_actions[ACTIONS.right] + cur_actions[ACTIONS.left]) * move_speed
+	velocity += acceleration
+	if(is_on_wall()):
+		velocity.x = 0
 	if(is_on_floor()):
 		velocity.y = 0
-	velocity += acceleration
+	move_and_slide(velocity, Vector2(0, -1))
+
+func _input(event):
+	if event.is_action("ui_right"):
+		if(event.pressed):
+			cur_actions[ACTIONS.right] = 1
+		else:
+			cur_actions[ACTIONS.right] = 0
+	if event.is_action("ui_left"):
+		if(event.pressed):
+			cur_actions[ACTIONS.left] = -1
+		else:
+			cur_actions[ACTIONS.left] = 0
