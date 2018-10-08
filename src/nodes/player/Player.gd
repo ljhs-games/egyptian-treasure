7b54f019976e3fa05926a7e4769bd4c21f281859
Cameron Reikes <cameronreikes@gmail.com>
2018-10-07 21:19:44 -0700

    Create various files for building the game

---
+++ src/nodes/player/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+enum ACTIONS { right, left }
+var cur_actions = { ACTIONS.right: 0, ACTIONS.left: 0 }
+
+export var move_speed = 300.0
+export var gravity = 9.8
+
+var velocity = Vector2()
+var acceleration = Vector2()
+
+func _ready():
+	acceleration.y = gravity
+	set_physics_process(true)
+
+func _physics_process(delta):
+	move_and_slide(velocity, Vector2(0, -1))
+	if(is_on_floor()):
+		velocity.y = 0
+	velocity += acceleration
