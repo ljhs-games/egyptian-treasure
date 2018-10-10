537df8265f487adcfd2829f987594216da5d3504
Cameron Reikes <cameronreikes@gmail.com>
2018-10-09 17:12:00 -0700

    Completely rework Player physics for RigidBody

--- src/nodes/player/Player.gd
+++ src/nodes/player/Player.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 enum ACTIONS { right, left }
 var cur_actions = { ACTIONS.right: 0, ACTIONS.left: 0 }
@@ -6,35 +6,22 @@ var cur_actions = { ACTIONS.right: 0, ACTIONS.left: 0 }
 export var move_acceleration = 30.0
 export var max_move_velocity = 450.0
 export var jump_velocity = 400.0
-export var move_drag = 50.0
-export var gravity = 16.8
 
-var on_floor = false
-var velocity = Vector2()
 var acceleration = Vector2()
 
 func _ready():
-	acceleration.y = gravity
 	set_physics_process(true)
 	set_process_input(true)
 
 func _physics_process(delta):
 	acceleration.x = (cur_actions[ACTIONS.right] + cur_actions[ACTIONS.left]) * move_acceleration
-	velocity += acceleration
-	velocity.x = clamp(velocity.x, -max_move_velocity, max_move_velocity)
-	if (cur_actions[ACTIONS.right] == 0) && (cur_actions[ACTIONS.left] == 0):
-		velocity.x += bring_to_zero(velocity.x, move_drag)
-	if(is_on_wall()):
-		velocity.x = 0
-	if(is_on_floor()):
-		velocity.y = 0
-	print(is_on_floor())
-	move_and_slide(velocity, Vector2(0, -1))
+	linear_velocity += acceleration
+	linear_velocity.x = clamp(linear_velocity.x, -max_move_velocity, max_move_velocity)
 
 func jump():
 	if($RayCast2D.is_colliding()):
 		#print(velocity.y)
-		velocity.y = -jump_velocity
+		linear_velocity.y = -jump_velocity
 		#print(velocity.y)
 
 func _input(event):
