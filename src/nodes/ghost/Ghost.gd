de0141dc275d32eba4b8af284d413589e390e278
Cameron Reikes <cameronreikes@gmail.com>
2018-10-18 19:07:20 -0700

    Give more functionality to ghost and player

--- src/nodes/ghost/Ghost.gd
+++ src/nodes/ghost/Ghost.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
-export var move_speed = 300.0
+enum DIRECTION { left, right }
+
+export var move_speed = 100.0
+
+var cur_direction = DIRECTION.right
 
 func _ready():
+	update_direction()
 	set_process(true)
 
 func _process(delta):
-	
+	update_direction()
+	if(cur_direction == DIRECTION.right):
+		global_position.x += move_speed*delta
+	if(cur_direction == DIRECTION.left):
+		global_position.x -= move_speed*delta
+
+func update_direction():
+	if(global_position.x > OS.get_real_window_size().x):
+		cur_direction = DIRECTION.left
+		$Sprite.flip_h = true
+	if(global_position.x < 0):
+		cur_direction = DIRECTION.right
+		$Sprite.flip_h = false
